5023813a69d4d473201782568a80e1ad058f48cd
vrgsl <aybuke.harmanci@virgosol.com>
2022-10-09 20:09:06 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+const MAXIMUM_NUMBER: u8 = 20;
+
+fn main() {
+    for n in 1..MAXIMUM_NUMBER {
+        println!("{}", n);
+        
+    }
+    //MAXIMUM_NUMBER == 30;  invalid, can't change the value of constant
+}
